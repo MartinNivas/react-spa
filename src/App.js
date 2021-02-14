e8fb3fd887e54ae2fd062db3edec146bf381cf6c
Nivas <arokiarajnivas@gmail.com>
2021-02-14 21:34:04 +0800

    Feat: Added endpoint for all 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from "react";
-import logo from './logo.PNG'
-import { Image, Input, Button, Radio, Table } from "semantic-ui-react";
+import React, { Component, useEffect, useState, useCallback } from "react";
+import logo from "./logo.PNG";
+import { Image, Input, Button, Radio, Table, Form } from "semantic-ui-react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  // Caution: Config section - starts
+
+  const baseUrl =
+    "https://g7qqvcisbf.execute-api.ap-southeast-1.amazonaws.com/dev";
+
+  const authToken = "Q92egB4D1W9pAj0v0kYbE6Lrp01WWlzFal3y545Y";
+
+  const getUsersListEndpoint = `${baseUrl}/`; // For displaying in the table view
+
+  const addUserEndpoint = `${baseUrl}/`;
+
+  const deleteUserEndpoint = `${baseUrl}/`;
+
+  const commonHeader = {
+    headers: {
+      "x-api-key": authToken,
+    },
+  };
+
+  // Config section - ends
+
+  const [users, setusers] = useState([]); //This state will be populated with the Axios HTTP response
+
+  const loadusers = useCallback(() => {
+    axios.get(getUsersListEndpoint, commonHeader).then((response) => {
+      setusers(response.data);
+    });
+  }, []);
+
+  //The users are loaded initially
+  useEffect(() => {
+    loadusers();
+  }, [loadusers]);
+
+  const onCreateNewuser = useCallback(
+    (newuserName) => {
+      axios
+        .post(
+          addUserEndpoint,
+          {
+            name: newuserName,
+          },
+          commonHeader
+        )
+        .then((result) => {
+          //Reload the user to show also the new one
+          loadusers();
+        })
+        .catch((error) => {
+          console.error("create new user error", error);
+        });
+    },
+    [loadusers]
+  );
+
   return (
     <div className="App">
       <Image src={logo} size="small" style={{ display: "block" }} />
-      <Input placeholder="First Name" style={{ marginRight: "1rem" }} />
-      <Input placeholder="Last Name" style={{ marginRight: "1rem" }} />
-      <Input
-        placeholder="DOB"
-        style={{ marginRight: "1rem", marginBottom: "3rem" }}
-      />
-      <Button size='Big'
-        style={{ display: "block", marginLeft: "50%", marginBottom: "3rem", backgroundColor: '#D5E8D4' }}
+      <Form
+        style={{ marginBottom: "3rem", maxWidth: "70%", marginLeft: "20%" }}
+      >
+        <Form.Group widths="equal">
+          <Form.Input size="large" placeholder="First Name" />
+          <Form.Input size="large" placeholder="Last Name" />
+          <Form.Input size="large" placeholder="DOB" />
+        </Form.Group>
+      </Form>
+      <Button
+        size="huge"
+        style={{
+          display: "block",
+          marginLeft: "50%",
+          marginBottom: "3rem",
+          backgroundColor: "#D5E8D4",
+        }}
       >
         Add
       </Button>
       {/* Table component - starts */}
 
-      <Table compact celled definition style={{maxWidth: '50%', marginLeft: '30%', backgroundColor: '#E2D5E7'}}>
+      <Table
+        compact
+        celled
+        definition
+        style={{
+          maxWidth: "50%",
+          marginLeft: "30%",
+          backgroundColor: "#E2D5E7",
+        }}
+      >
         <Table.Body>
           <Table.Row>
             <Table.Cell collapsing>
@@ -30,7 +104,7 @@ function App() {
             <Table.Cell>September 14, 2013</Table.Cell>
             <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
           </Table.Row>
-          <Table.Row>
+          {/* <Table.Row>
             <Table.Cell collapsing>
               <Radio toggle />
             </Table.Cell>
@@ -45,12 +119,18 @@ function App() {
             <Table.Cell>Jill Lewis</Table.Cell>
             <Table.Cell>May 11, 2014</Table.Cell>
             <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
-          </Table.Row>
+          </Table.Row> */}
         </Table.Body>
       </Table>
 
-      <Button size='Big'
-        style={{ display: "block", marginLeft: "50%", marginTop: "3rem", backgroundColor: '#D5E8D4' }}
+      <Button
+        size="huge"
+        style={{
+          display: "block",
+          marginLeft: "50%",
+          marginTop: "3rem",
+          backgroundColor: "#D5E8D4",
+        }}
       >
         Delete
       </Button>
